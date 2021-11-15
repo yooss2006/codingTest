ad79ff8c80e51cf4a5d74283c81bc981e197bb96
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2021-11-15 15:33:14 +0900

    Add files via upload

---
+++ 1lv_문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,11 @@
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    if (a[n] == b[n]) {
+      return a > b ? 1 : -1;
+    } else {
+      return a[n] > b[n] ? 1 : -1;
+    }
+  });
+}
+
+console.log(solution(["sun", "bed", "car"], 1));
