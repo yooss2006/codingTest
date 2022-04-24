40288e48893d77b75170bac922f5ec5530f68ebd
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-04-24 16:10:15 +0900

    Add files via upload

---
+++ 순열구하기.js
@@ -0,0 +1,25 @@
+function solution(m, arr) {
+  let answer = [];
+  let ch = Array.from({ length: arr.length }, () => 0);
+  let tmp = Array.from({ length: m }, () => 0);
+
+  function DFS(L) {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          tmp[L] = arr[i];
+          DFS(L + 1);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  DFS(0);
+  return answer.length;
+}
+
+let arr = [3, 6, 9];
+console.log(solution(2, arr));
