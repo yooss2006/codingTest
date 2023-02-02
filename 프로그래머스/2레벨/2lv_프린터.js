dd4178e94744e0bec7192c37778f42ae0b8caed3
yooss2006 <yooss2006@naver.com>
2023-02-02 12:15:37 +0900

    프로그래머스 2단계 스택/큐 프린터

---
+++ 프로그래머스/2레벨/2lv_프린터.js
@@ -0,0 +1,15 @@
+function solution(priorities, location) {
+  let answer = 0;
+  const queue = priorities.map((priority, index) => ({ index, priority }));
+  while (true) {
+    const numberQueue = queue.map((item) => item.priority);
+    if (Math.max(...numberQueue) === queue[0].priority) {
+      answer++;
+      if (queue[0].index === location) break;
+    } else queue.push(queue[0]);
+    queue.shift();
+  }
+  return answer;
+}
+
+console.log(solution([1, 1, 9, 1, 1, 1], 0));
