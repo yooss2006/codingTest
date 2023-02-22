fa65c201f65f996b331f9b10d85c0a1772826da2
yooss2006 <yooss2006@naver.com>
2023-02-22 18:44:47 +0900

    프린터

--- 프로그래머스/2레벨/2lv_프린터.js
+++ 프로그래머스/2레벨/2lv_프린터.js
@@ -2,7 +2,7 @@ function solution(priorities, location) {
   let answer = 0;
   const queue = priorities.map((priority, index) => ({ index, priority }));
   while (true) {
-    const numberQueue = queue.map((item) => item.priority);
+    const numberQueue = queue.map(({ priority }) => priority);
     if (Math.max(...numberQueue) === queue[0].priority) {
       answer++;
       if (queue[0].index === location) break;
